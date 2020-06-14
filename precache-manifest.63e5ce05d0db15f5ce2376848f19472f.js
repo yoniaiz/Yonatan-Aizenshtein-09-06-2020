86e4079cbeb51f6e6b9f381bc04f71812eeebbb9
yoniaiz <yoni29396@gmail.com>
2020-06-14 15:28:34 +0300

    Updates

---
+++ precache-manifest.63e5ce05d0db15f5ce2376848f19472f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ecc788dd39a7f645907690d16f81e8",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/index.html"
+  },
+  {
+    "revision": "952d4775967a06d98099",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/css/3.3a221eca.chunk.css"
+  },
+  {
+    "revision": "65d3f9732a984d722e46",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/js/0.dc0e8c6e.chunk.js"
+  },
+  {
+    "revision": "952d4775967a06d98099",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/js/3.dab0f8a2.chunk.js"
+  },
+  {
+    "revision": "492d7e05a4c2e81da7932422a4d31130",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/js/3.dab0f8a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06267abec2ac341076e",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/js/4.d8269785.chunk.js"
+  },
+  {
+    "revision": "0365dd01981553136912",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/js/5.d9cb718c.chunk.js"
+  },
+  {
+    "revision": "746331ff5c1aed71a992",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/js/6.318e77e7.chunk.js"
+  },
+  {
+    "revision": "c7f5730bcb897987a681",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/js/main.97a7b907.chunk.js"
+  },
+  {
+    "revision": "283e75b67c723dc13e16",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/js/runtime-main.e563d5b6.js"
+  },
+  {
+    "revision": "5bc2727ad1c26d4ac11530e134c559ca",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/media/hearIconOutline.5bc2727a.png"
+  },
+  {
+    "revision": "7b36ad43d8486841aa896938c88ea689",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/media/heartIcon.7b36ad43.png"
+  },
+  {
+    "revision": "5eceaf6b497861227cb6681dfc271265",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/media/logo.5eceaf6b.png"
+  },
+  {
+    "revision": "41a17015ae66b228fc8e6d2c04207c6d",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/media/moon.41a17015.png"
+  },
+  {
+    "revision": "bc406e794924738571446c657c5be4ac",
+    "url": "/Yonatan-Aizenshtein-09-06-2020/static/media/sun.bc406e79.png"
+  }
+]);
