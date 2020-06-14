0ff437c1aac202d1dd5883fcb2cd42131d6f5a5f
yoniaiz <yoni29396@gmail.com>
2020-06-14 11:01:29 +0300

    files cleanup

--- src/components/Background/Lottie/Moon.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import moon from "assets/animations/moon.json";
-import LottieLoader from "utils/LottieLoader";
-
-export default () => {
-  return <LottieLoader animationJson={moon} classes={"moon"} />;
-};
